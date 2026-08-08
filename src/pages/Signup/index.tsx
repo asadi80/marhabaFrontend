cef6b7a3421b059fe88926f39789c45e116ff5e1
Abdurraouf <a.k.sadi80@gmail.com>
2026-08-08 00:22:27 -0700

    updating

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -176,7 +176,7 @@ export default function Signup() {
   setLoading(true);
   try {
     const res = await fetch(
-      "https://marhababackend.onrender.com/api/v1/auth/register",
+      "https://marhaba.ly/api/v1/auth/register",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
